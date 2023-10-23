33151e01fb1097122f6922870f0b6fa9fd5eaa54
shivakshi-07 <148673962+shivakshi-07@users.noreply.github.com>
2023-10-23 14:49:39 +0530

    Update TT.py

--- TT.py
+++ TT.py
@@ -1,8 +1,15 @@
-#TIMETABLE PROJECT
+#TIMETABLE PROJECT-THIS SHIT SUCKS! PIKA-PIKA!--EVEN MY PIKACHU AGREES:)
 from tkinter import *
+from tkinter import ttk
 root=Tk()
 root.title('Automated timetable maker')
 root.geometry("900x400')
 root.minsize(900,400)
+sub=ttk.Button(root,text='ADD SUBJECT')
+sub.place(x=1,y=2)
+teacher=ttk.Button(root,text='ADD TEACHER')
+teacher.place(x=100,y=2)
+profile=ttk.Button(root,text='INFO') #PLS CHANGE THE SHAPE TO THAT OF A CIRCLE
+profile.place(x=1200,y=2)
 root.mainloop()
 
