eeb3bc9a0ef666a0bec0d5acebec161600b0bb60
Pavani <147927096+Uffcomeon@users.noreply.github.com>
2023-10-24 09:43:37 +0530

    Update TT.py

--- TT.py
+++ TT.py
@@ -3,13 +3,25 @@ from tkinter import *
 from tkinter import ttk
 root=Tk()
 root.title('Automated timetable maker')
-root.geometry("900x400')
+root.geometry('900x400')
 root.minsize(900,400)
-sub=ttk.Button(root,text='ADD SUBJECT')
+def s():
+    window=Tk()
+    window.title('subject selection')
+    window.geometry('400x250')
+    l1=Label(window,text='select your subject from the below drop down list')
+    l1.place(x=20,y=5)
+    i=StringVar()
+    list1=['Maths','Physics','Chemistry','Biology','Computer science']
+    sub=OptionMenu(window,i,*list1)
+    i.set('select subject')
+    sub.config(width=15)
+    sub.place(x=50,y=50)
+    
+sub=ttk.Button(root,text='ADD SUBJECT',command=s)
 sub.place(x=1,y=2)
 teacher=ttk.Button(root,text='ADD TEACHER')
-teacher.place(x=100,y=2)
+teacher.place(x=150,y=5)
 profile=ttk.Button(root,text='INFO') #PLS CHANGE THE SHAPE TO THAT OF A CIRCLE
-profile.place(x=1200,y=2)
+profile.place(x=800,y=2)
 root.mainloop()
-
